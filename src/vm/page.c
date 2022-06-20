b3643f12765410095450144bc2e2bcd0c82b4e96
Jaegoomon <jaegoo1199@gmail.com>
2022-06-20 13:24:42 +0900

    fix: page util functions
    * pointer 사용 주의
    * 오탈자 수정

--- src/vm/page.c
+++ src/vm/page.c
@@ -20,10 +20,10 @@ void vm_destory(struct hash *vm)
 
 struct vm_entry *find_vme(void *vaddr)
 {
-    struct hash vm = thread_current()->vm;
+    struct hash *vm = &thread_current()->vm;
     struct hash_iterator i;
 
-    hash_first(&i, &vm);
+    hash_first(&i, vm);
     while (hash_next(&i))
     {
         struct vm_entry *vme = hash_entry(hash_cur(&i), struct vm_entry, elem);
@@ -67,9 +67,9 @@ static unsigned vm_hash_func(const struct hash_elem *e, void *aux)
 static bool vm_less_func(const struct hash_elem *a, const struct hash_elem *b, void *aux)
 {
     struct vm_entry *vme_a = hash_entry(a, struct vm_entry, elem);
-    struct vm_entry *vme_b = hash_entry(a, struct vm_entry, elem);
+    struct vm_entry *vme_b = hash_entry(b, struct vm_entry, elem);
 
-    if (vme_a->vaddr <= vme_b->vaddr)
+    if (vme_a->vaddr < vme_b->vaddr)
         return true;
 
     return false;
